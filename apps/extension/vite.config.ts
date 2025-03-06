cd121f293a1fd369defbebd5635fcd8916fa4a73
Thanh Nguyen <lethanh300504@gmail.com>
2025-03-06 20:09:08 +1100

    remove port 3001 in extension

--- apps/extension/vite.config.ts
+++ apps/extension/vite.config.ts
@@ -4,9 +4,6 @@ import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 
 export default defineConfig({
-  server: {
-    port: 3001,
-  },
   plugins: [
     react(),
     crx({ manifest }),
