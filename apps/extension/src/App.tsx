d66c22fec2100c4814bf386a1220a8a49b1f73b7
Thanh Nguyen <lethanh300504@gmail.com>
2025-03-07 23:08:17 +1100

    delete sync button

--- apps/extension/src/App.tsx
+++ apps/extension/src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
-import SyncButton from './components/SyncButton';
 
 function App() {
   return (
     <>
       <h1>Linkedin Scraper Extension</h1>
-      <SyncButton />
     </>
   )
 }
--- apps/extension/src/components/SyncButton.tsx
+++
@@ -1,9 +0,0 @@
-const SyncButton = () => {
-  return (
-    <button className="bg-blue-500 text-white p-2 rounded-md">
-      Sync
-    </button>
-  );
-}
-
-export default SyncButton;
